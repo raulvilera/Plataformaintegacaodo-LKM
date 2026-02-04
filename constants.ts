edb2a5f3380c5ddf01d0804377156a2ae23ea4f9
Raul <raulvilera@gmail.com>
2026-02-04 08:11:35 -0300

    feat: standardize and sort class lists in constants file

---
+++ constants.ts
@@ -0,0 +1,13 @@
+
+export const DATA_TURMAS = [
+    // Ensino Fundamental II
+    '6ºAno A', '6ºAno B', '6ºAno C', '6ºAno D', '6ºAno E', '6ºAno F',
+    '7ºAno A', '7ºAno B', '7ºAno C', '7ºAno D', '7ºAno E', '7ºAno F',
+    '8ºAno A', '8ºAno B', '8ºAno C', '8ºAno D', '8ºAno E',
+    '9ºAno A', '9ºAno B', '9ºAno C', '9ºAno D',
+
+    // Ensino Médio
+    '1ª Série A', '1ª Série B', '1ª Série C', '1ª Série D', '1ª Série E', '1ª Série F',
+    '2ª Série A', '2ª Série B', '2ª Série C', '2ª Série D', '2ª Série E', '2ª Série F', '2ª Série G', '2ª Série H',
+    '3ª Série A', '3ª Série B', '3ª Série C', '3ª Série D', '3ª Série E', '3ª Série F', '3ª Série G', '3ª Série H'
+];
